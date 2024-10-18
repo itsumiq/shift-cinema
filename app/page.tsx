69da2fc5050d94f3852ae35cf380c735506ab4a7
itsumi <122093200+itsumiq@users.noreply.github.com>
2024-10-18 03:19:24 +0300

    style: add border for desktop header

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import { CardList } from "@/src/widgets/cardList";
 
 const Home = (): JSX.Element => {
   return (
-    <>
+    <div className="mt-4 lg:mt-12">
       <CardList />
-    </>
+    </div>
   );
 };
 
--- src/widgets/headers/ui/Header.tsx
+++ src/widgets/headers/ui/Header.tsx
@@ -3,7 +3,7 @@ import { HeaderDesktop } from "./HeaderDesktop";
 
 export const Header = (): JSX.Element => {
   return (
-    <header>
+    <header className="mx-auto w-[91.1112vw] lg:w-[62.5vw] desktop:w-[66.667vw]">
       <HeaderMobile />
       <HeaderDesktop />
     </header>
