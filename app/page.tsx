881ff1f7eaee3fec28927879611d2c66c702e996
itsumi <122093200+itsumiq@users.noreply.github.com>
2024-10-18 19:05:26 +0300

    feat: add metadata on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 import { CardList } from "@/src/widgets/cardList";
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "SHIFT CINEMA | Афиша",
+  description: "List of films available for ticket purchase",
+};
+
 const Home = (): JSX.Element => {
   return (
     <div className="mt-4 lg:mt-12">
